b0d60886424c717a93c16994c69c610d5b6ac658
AmosLipsys <AmosLipsys@users.noreply.github.com>
2016-05-03 03:06:49 +1000

    Convert to use bootstrap
    Made the Home Page (Mostly Finished)
    Added a carousel for news and announcements
    Created all other pages(Not made just for testing links)
    
    No Photos Uploaded

---
+++ navbar.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+&lt;?php<br>
+echo &quot; &lt;!-- NAVBAR\n&quot;; <br>
+echo &quot;    ================================================== --&gt;\n&quot;; <br>
+echo &quot;    &lt;div class=\&quot;navbar-wrapper\&quot;&gt;\n&quot;; <br>
+echo &quot;      &lt;div class=\&quot;container\&quot;&gt;\n&quot;; <br>
+echo &quot;\n&quot;; <br>
+echo &quot;        &lt;nav class=\&quot;navbar navbar-inverse navbar-static-top\&quot;&gt;\n&quot;; <br>
+echo &quot;          &lt;div class=\&quot;container\&quot;&gt;\n&quot;; <br>
+echo &quot;            &lt;div class=\&quot;navbar-header\&quot;&gt;\n&quot;; <br>
+echo &quot;              &lt;button type=\&quot;button\&quot; class=\&quot;navbar-toggle collapsed\&quot; data-toggle=\&quot;collapse\&quot; data-target=\&quot;#navbar\&quot; aria-expanded=\&quot;false\&quot; aria-controls=\&quot;navbar\&quot;&gt;\n&quot;; <br>
+echo &quot;                &lt;span class=\&quot;sr-only\&quot;&gt;Toggle navigation&lt;/span&gt;\n&quot;; <br>
+echo &quot;                &lt;span class=\&quot;icon-bar\&quot;&gt;&lt;/span&gt;\n&quot;; <br>
+echo &quot;                &lt;span class=\&quot;icon-bar\&quot;&gt;&lt;/span&gt;\n&quot;; <br>
+echo &quot;                &lt;span class=\&quot;icon-bar\&quot;&gt;&lt;/span&gt;\n&quot;; <br>
+echo &quot;              &lt;/button&gt;\n&quot;; <br>
+echo &quot;              &lt;a class=\&quot;navbar-brand\&quot; href=\&quot;#\&quot;&gt;&lt;img class=\&quot;big_logo\&quot; src=\&quot;photos/bro-logo2.png\&quot;  alt=\&quot;BRO Logo\&quot;/&gt;&lt;/a&gt;\n&quot;; <br>
+echo &quot;              &lt;a class=\&quot;navbar-brand\&quot; href=\&quot;#\&quot;&gt;&lt;img class=\&quot;small_logo\&quot; src=\&quot;photos/bro-logo2-small.png\&quot;  alt=\&quot;BRO Logo\&quot;/&gt;&lt;/a&gt;\n&quot;; <br>
+echo &quot;            &lt;/div&gt;\n&quot;; <br>
+echo &quot;            &lt;div id=\&quot;navbar\&quot; class=\&quot;navbar-collapse collapse\&quot;&gt;\n&quot;; <br>
+echo &quot;              &lt;ul class=\&quot;nav navbar-nav\&quot;&gt;\n&quot;; <br>
+echo &quot;                &lt;li&gt;&lt;a href=\&quot;index.html\&quot;&gt;Home &lt;span class=\&quot;glyphicon glyphicon-home\&quot; aria-hidden=\&quot;true\&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                &lt;li&gt;&lt;a href=\&quot;#about\&quot;&gt;Want a Ticket?&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                &lt;li&gt;&lt;a href=\&quot;#contact\&quot;&gt;Become A Patron&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                \n&quot;; <br>
+echo &quot;                &lt;li class=\&quot;dropdown\&quot;&gt;\n&quot;; <br>
+echo &quot;                  &lt;a href=\&quot;#\&quot; class=\&quot;dropdown-toggle\&quot; data-toggle=\&quot;dropdown\&quot; role=\&quot;button\&quot; aria-haspopup=\&quot;true\&quot; aria-expanded=\&quot;false\&quot;&gt;Want more Info?&lt;span class=\&quot;caret\&quot;&gt;&lt;/span&gt;&lt;/a&gt;\n&quot;; <br>
+echo &quot;                  &lt;ul class=\&quot;dropdown-menu\&quot;&gt;\n&quot;; <br>
+echo &quot;                    &lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Hot off the Press&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                    &lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Our Legacy&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                    &lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Honorary Members&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                    &lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Say Cheese &lt;span class=\&quot;glyphicon glyphicon-camera\&quot; aria-hidden=\&quot;true\&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                  &lt;/ul&gt;                  \n&quot;; <br>
+echo &quot;                &lt;/li&gt;\n&quot;; <br>
+echo &quot;                \n&quot;; <br>
+echo &quot;                &lt;li class=\&quot;dropdown\&quot;&gt;\n&quot;; <br>
+echo &quot;                  &lt;a href=\&quot;#\&quot; class=\&quot;dropdown-toggle\&quot; data-toggle=\&quot;dropdown\&quot; role=\&quot;button\&quot; aria-haspopup=\&quot;true\&quot; aria-expanded=\&quot;false\&quot;&gt;Members Only&lt;span class=\&quot;caret\&quot;&gt;&lt;/span&gt;&lt;/a&gt;\n&quot;; <br>
+echo &quot;                  &lt;ul class=\&quot;dropdown-menu\&quot;&gt;\n&quot;; <br>
+echo &quot;                    &lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Staff Room&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                    &lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Newsleters&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;&lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Rules and Regulations&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                    &lt;li&gt;&lt;a href=\&quot;#\&quot;&gt;Report&lt;span class=\&quot;glyphicon glyphicon-camera\&quot; aria-hidden=\&quot;true\&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;                  &lt;/ul&gt;                  \n&quot;; <br>
+echo &quot;                &lt;/li&gt;\n&quot;; <br>
+echo &quot;                \n&quot;; <br>
+echo &quot;              &lt;li&gt;&lt;a href=\&quot;#contact\&quot;&gt;Contact Us &lt;span class=\&quot;glyphicon glyphicon-envelope\&quot; aria-hidden=\&quot;true\&quot;&gt;&lt;/span&gt;&lt;/a&gt;&lt;/li&gt;\n&quot;; <br>
+echo &quot;              &lt;/ul&gt;\n&quot;; <br>
+echo &quot;            &lt;/div&gt;\n&quot;; <br>
+echo &quot;          &lt;/div&gt;\n&quot;; <br>
+echo &quot;        &lt;/nav&gt;\n&quot;; <br>
+echo &quot;\n&quot;; <br>
+echo &quot;      &lt;/div&gt;\n&quot;; <br>
+echo &quot;    &lt;/div&gt;\n&quot;; <br>
+echo &quot;\n&quot;;<br>
+?&gt;
+</body>
+</html>
